45236ea305624b01ae00e29fdd972c442a86bb5e
szuluaga-beep <69664305+szuluaga-beep@users.noreply.github.com>
2020-11-27 18:55:47 -0500

    CreateDB

---
+++ Onsale.Common/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onsale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
